b874a1f82ef456c25d5d8f55c515cebaf4eb616b
silvalazaro <lazarofdsilva@gmail.com>
2020-06-12 00:27:12 -0300

    Criacao do componente Formulario

---
+++ src/components/formulario/Formulario.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <q-form class="q-gutter-md">
+      <q-input
+        filled
+        label="Seu nome"
+      />
+    </q-form>
+
+  </div>
+</template>
+
+<script>
+import { QInput } from 'quasar'
+export default {
+  // name: 'ComponentName',
+  props: ['fields'],
+  data () {
+    return {}
+  },
+  methods: {
+    getField () {
+      return new QInput()
+    }
+  },
+  created () {
+    let a
+  }
+}
+</script>
